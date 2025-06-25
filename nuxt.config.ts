5fb70f0ff9a8bd34a5b81bf5f405f5ab4632e9a8
Qiu Jun <qiujun2004@outlook.com>
2025-06-26 03:20:05 +0800

    feat: install nuxt-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: [
     'nuxt-mongoose',
+    '@nuxt/ui',
   ],
   mongoose: {
     uri: process.env.MONGODB_URI,
     options: {},
     modelsDir: 'models',
     devtools: true,
+  },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 })
