721f77fb009e1dcac633933ea253c27ecfc0603b
Qiu Jun <qiujun2004@outlook.com>
2025-06-26 19:58:27 +0800

    refactor: index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,52 @@
 <template>
   <div>
-    <index-el-autocomplete v-model="name" />
+    <el-autocomplete
+      v-model="name"
+      :fetch-suggestions="search"
+      placeholder="请输入你的用户名..."
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { Player } from '~/types/player'
+
 const name = ref('')
+
+interface NameSelectItem {
+  value: string
+}
+
+const names = ref<NameSelectItem[]>([])
+
+async function loadNames() {
+  try {
+    const result = await $fetch<Player[]>(`/api/v1/players`, {
+      method: 'GET',
+    })
+    
+    names.value = result.map(player => ({ value: player.name }))
+  } catch (error: any) {
+    ElMessage.error(`Error fetching players: ${error.statusMessage || 'Unknown error'}`)
+    names.value = []
+  }
+}
+
+function search(queryString: string) {
+  const results = queryString
+    ? names.value.filter((restaurant: NameSelectItem) => {
+      return (
+        restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+      )
+    })
+    : names.value
+
+  return results
+}
+
+onMounted(async () => {
+  await loadNames()
+})
 </script>
 
 <style>
