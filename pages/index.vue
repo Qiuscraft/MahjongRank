837449d5ebe24381e0532bcc1cad43df0811cfba
Qiu Jun <qiujun2004@outlook.com>
2025-06-26 11:08:38 +0800

    feat: searchMenu

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,48 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <UInputMenu 
+      v-model="name" 
+      v-model:open="open"
+      :items="names" 
+      @focus="open = true" 
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { NuxtWelcome } from '#components';
+interface Player {
+  _id: string;
+  name: string;
+}
 
+const names = ref<string[]>([])
+const name = ref<string>('')
+const open = ref<boolean>(false)
+
+const searchName = async () => {
+  try {
+    const result = await $fetch<Player[]>(`/api/v1/players`, {
+      method: 'GET',
+      params: {
+        search_name: name.value,
+      }
+    });
+    
+    names.value = result.map(player => player.name);
+    
+  } catch (error: any) {
+    const toast = useToast()
+    toast.add({
+      title: 'Failed to fetch player names.',
+      description: error.statusMessage || 'Unknown error',
+      color: 'error',
+    });
+  }
+}
+
+onMounted(async () => {
+  await searchName();
+});
 
 </script>
 
