e03093f4daa5d77cdf77adb3f585ce5e104938cc
Qiu Jun <qiujun2004@outlook.com>
2025-06-26 13:02:49 +0800

    feat: 重新使用elementplus实现搜索框

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,66 @@
 <template>
   <div>
-    
+    <el-autocomplete
+      v-model="name"
+      :fetch-suggestions="querySearchAsync"
+      placeholder="请输入你的用户名..."
+      @select="handleSelect"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-const names = ref<string[]>([])
-const name = ref<string>('')
+interface NameItem {
+  value: string
+}
 
-const search = async (searchName: string) => {
+interface Player {
+  name: string
+}
+
+const name = ref<string>('')
+const names = ref<NameItem[]>([])
+
+const loadAll = async (): Promise<NameItem[]> => {
   try {
-    const result = await $fetch(`/api/v1/players`, {
+    const result = await $fetch<Player[]>(`/api/v1/players`, {
       method: 'GET',
-      params: {
-        search_name: searchName,
-      }
     });
     
-    names.value = result.map(player => player.name);
-    
+    return result.map(player => ({ value: player.name }));
   } catch (error) {
-    console.error('Error fetching players:', error);
+    ElMessage.error(`Error fetching players: ${error}`);
+    return [];
   }
 }
 
+let timeout: ReturnType<typeof setTimeout>
+const querySearchAsync = (queryString: string, cb: (arg: any) => void) => {
+  const results = queryString
+    ? names.value.filter(createFilter(queryString))
+    : names.value
+
+  clearTimeout(timeout)
+  timeout = setTimeout(() => {
+    cb(results)
+  }, 0)
+  // 已经预加载过的数据，不需要延迟
+}
+const createFilter = (queryString: string) => {
+  return (restaurant: NameItem) => {
+    return (
+      restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+    )
+  }
+}
+
+const handleSelect = (item: Record<string, any>) => {
+  console.log(item)
+}
+
 onMounted(async () => {
-  await search(name.value);
-});
+  names.value = await loadAll()
+})
 
 </script>
 
