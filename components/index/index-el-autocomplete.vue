738fe9d020ac3b04da35dcb49e7746965df0fdbb
Qiu Jun <qiujun2004@outlook.com>
2025-06-26 19:15:21 +0800

    refactor: index界面组件提取

---
+++ components/index/index-el-autocomplete.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <el-autocomplete
+      v-model="name"
+      :fetch-suggestions="querySearchAsync"
+      placeholder="请输入你的用户名..."
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface NameItem {
+  value: string
+}
+
+interface Player {
+  name: string
+}
+
+const name = defineModel<string>({ default: '' })
+const names = ref<NameItem[]>([])
+
+const loadAll = async (): Promise<NameItem[]> => {
+  try {
+    const result = await $fetch<Player[]>(`/api/v1/players`, {
+      method: 'GET',
+    });
+    
+    return result.map(player => ({ value: player.name }));
+  } catch (error: any) {
+    ElMessage.error(`Error fetching players: ${error.statusMessage || 'Unknown error'}`);
+    return [];
+  }
+}
+
+let timeout: ReturnType<typeof setTimeout>
+const querySearchAsync = (queryString: string, cb: (arg: any) => void) => {
+  const results = queryString
+    ? names.value.filter(createFilter(queryString))
+    : names.value
+
+  clearTimeout(timeout)
+  timeout = setTimeout(() => {
+    cb(results)
+  }, 0)
+  // 已经预加载过的数据，不需要延迟
+}
+const createFilter = (queryString: string) => {
+  return (restaurant: NameItem) => {
+    return (
+      restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+    )
+  }
+}
+
+onMounted(async () => {
+  names.value = await loadAll()
+})
+
+</script>
+
+<style>
+
+</style>
